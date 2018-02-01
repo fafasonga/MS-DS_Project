4c0439002f27f705e7cbed77a92279789aa7dbf6
fafasonga <s.fabrice@innopolis.ru>
2018-02-01 21:07:54 +0300

    Test

--- app/routes.py
+++ app/routes.py
@@ -5,32 +5,34 @@ from app.models import Location, Base, AlchemyEncoder, User
 import json
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
+    if request.method == 'POST':
+        print("Got new request")
+        username = request.form["username"]
+        print("username is: ", username)
 
+        user = session.query(User).filter_by(username=username).first()
+        print("User is: ", user)
+
+        if user is None:
+            if username != "all":
+                raise Exception("No user found!")
+
+        locations = session.query(Location)
+        if username == "all":
+            locations = locations.all()
+        else:
+            locations = locations.filter_by(user_id=user.id).all()
+        locations = json.dumps(locations, cls=AlchemyEncoder)
+    else:
+        locations = ""
     users = session.query(User).all()
 
-    return render_template('index.html', users=users)
+    return render_template("index.html", data=locations, users=users)
 
 
-@app.route("/fetch_data", methods=['POST'])
-def fetch_data():
-    print("Got new request")
-    print(request.json)
-    username = request.json["username"]
-    print("username is: ", username)
-
-    user = session.query(User).filter_by(username=username).first()
-    print("User is: ", user)
-
-    if user is None:
-        if username != "all":
-            raise Exception("No user found!")
-
-    locations = session.query(Location)
-    if username == "all":
-        locations = locations.all()
-    else:
-        locations = locations.filter_by(user_id=user.id).all()
-
-    return json.dumps(locations, cls=AlchemyEncoder)
+@app.route("/table")
+def fill_table():
+    locations = session.query(Location, User).join(User).add_columns(User.username, Location.id, Location.lat, Location.lan, Location.timestamp).all()
+    return render_template('tables.html', data=locations)
