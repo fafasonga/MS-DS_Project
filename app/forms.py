f827695fe41659310d6791e621f91fce1b540070
fafasonga <s.fabrice@innopolis.ru>
2018-05-04 17:36:03 +0300

    MS - DS Thesis

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.fields import FloatField
-from wtforms.fields.html5 import DateTimeLocalField
+from wtforms.fields.html5 import DateTimeField
 from wtforms.validators import DataRequired
 
 from app import session, User, settings
@@ -15,6 +15,6 @@ def get_users():
 class LocationForm(FlaskForm):
     lat = FloatField('Latitude', validators=[DataRequired()], render_kw={"placeholder": "46.22222"})
     lan = FloatField('Longitude', validators=[DataRequired()], render_kw={"placeholder": "11.2222"})
-    timestamp = DateTimeLocalField('Timestamp', validators=[DataRequired()], format=settings.DATETIME_FORMAT, render_kw={"placeholder": "30/12/2016 23:59:01"})
+    timestamp = DateTimeField('Timestamp', validators=[DataRequired()], format=settings.DATETIME_FORMAT, render_kw={"placeholder": "30/12/2016 23:59:01"})
     user_id = SelectField('User', choices=get_users(), coerce=int)
     submit = SubmitField('Submit')
